c8f33fa0d7e65aa8498230b54c5dd27d77bec4f2
Travis Swicegood <development@domain51.com>
2014-11-21 14:21:08 -0800

    First pass at a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='continuum-flask',
+    version='1.0.0',
+    author='Continuum Analytics',
+    author_email='support@continuum.io',
+    description="Continuum Analytics' customized Flask",
+    packages=[
+        'continuum_flask',
+    ],
+)
